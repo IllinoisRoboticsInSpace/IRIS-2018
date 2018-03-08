6c1f4cad1dd40531d9404795dd73a1656fc5ab23
Roger Q <twofs0.5@gmail.com>
2018-03-07 20:23:46 -0600

    add CMakeList and organize code for a bit

---
+++ kinect/README.md
@@ -0,0 +1,8 @@
+Instruction to use the code
+```
+cd build
+cmake ..
+make
+./mapping_test
+./kinect_test
+```
---
+++ kinect/test_main.cpp
@@ -0,0 +1,8 @@
+#include "mapping/mapping.h"
+#include "test2/kinectDevice.h"
+#include <iostream>
+
+int main(void){
+    std::cout << "I'm doing nothing for now" << std::endl;
+    return 0;
+}
