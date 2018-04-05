cdb245b5cd425ab5b81c9c70c1c364e5885b4dd0
Yeda Wu <Yeda@wirelessprv-10-194-87-96.near.illinois.edu>
2018-04-04 22:21:02 -0500

    Added servos code

--- arduinocode/IRIS_Motor.ino
+++ arduinocode/IRIS_Motor.ino
@@ -1,23 +1,30 @@
+#include <Servo.h>
+
+Servo myservo;
 String command;
 int value;
+int pos = 0;
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial.begin(115200);
+  Serial.setTimeout(50);
+  Serial1.begin(115200);
   Serial1.write(0);
   Serial1.write(52);
   Serial1.write(3);
+  myservo.attach(9);
 }
 
 void loop() {
   if (Serial.available()) { 
     String s = Serial.readString();
     int index = s.indexOf("/");
+    int index2 = s.indexOf("/",index+1);
     command = s.substring(0,index);
-    value = s.substring(index+1).toInt();
+    value = s.substring(index+1, index2).toInt();
+    pos = s.substring(index2+1).toInt()/2;
+    myservo.write(pos);
   }
-
-  Serial.print(command);
   
   if (command == "go"){
     Serial1.write(0);
@@ -27,7 +34,7 @@ void loop() {
     Serial1.write(50);
     Serial1.write(0);
   }
-  
+
   if (command == "stop"){
     Serial1.write(0);
     Serial1.write(49);
