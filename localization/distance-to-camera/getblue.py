17eed4074c5f162f70b147bf20d284d85312cc32
Dhruv Patel <akido2398gmail.com>
2018-04-05 20:33:17 -0500

    Added last years code, and modified it

--- localization/distance-to-camera/getblue.py
+++ localization/distance-to-camera/getblue.py
@@ -32,5 +32,28 @@ def returnFrame():
 	#cv2.imshow('resb', resb)
 	#if(cv2.waitKey(1) & 0xFF == ord('q')):
 	#	break
+def UseImage(frame):
+	#ret, frame = cap.read()
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower_red = np.array([110,50,50])
+	upper_red = np.array([130, 255, 255])
+
+	#lower_black = np.array([110,50,50])
+	#upper_black = np.array([130, 255, 255])
+	
+	#orange = np.uint8([[[255,144,69]]])
+	#print( cv2.cvtColor(upper_red, cv2.COLOR_HSV2BGR))
+	#hsv_orange = cv2.cvtColor(black, cv2.COLOR_BGR2HSV)
+
+	#print hsv_orange
+
+	masko = cv2.inRange(hsv,np.array([0,50,50]),np.array([3,255,255]))
+	#masku = cv2.inRange(hsv, np.array([98,50,50]), np.array([118,255,255]))
+	masku = cv2.inRange(hsv, np.array([100,45,100]), np.array([150,255,255]))
+	
+	resb = cv2.bitwise_and(frame, frame, mask=masku)
+	reso = cv2.bitwise_and(frame,frame,mask=masko)
+
+	return resb
 
 
