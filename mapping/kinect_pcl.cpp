e53000da9d556dff349eafd504a64d008f14e8b6
Roger Q <twofs0.5@gmail.com>
2018-04-10 20:21:35 -0500

    fixed namespace

--- mapping/kinect_pcl.cpp
+++ mapping/kinect_pcl.cpp
@@ -2,12 +2,12 @@
 #include "CoordSystemKinect.hpp"
 
 std::vector<cvec3f> get_point_cloud(cv::Mat * cur_depth_frame){
-    uint8_t* pixelPtr = (uint8_t*)cur_depth_frame.data;
+    uint8_t* pixelPtr = (uint8_t*)cur_depth_frame->data;
     int point_count = cur_depth_frame->cols * cur_depth_frame->rows;
     std::vector<cvec3f> ret;
     for(int row = 0; row < cur_depth_frame->rows; row++){
         for(int col = 0; col < cur_depth_frame->cols; col++){
-            float distance = csk::RawDepthToMilli(pixelPtr[csk::GetIndex(col, row)]);
+            float distance = CSK::RawDepthToMilli(pixelPtr[CSK::GetIndex(col, row)]);
             if(distance > maxViewDist || distance < minViewDist){
                 //ret.push_back(cvec3f(0, 0, 0));
                 //if the data is not valid, drop it (or edit the code to put some sentinel values)
