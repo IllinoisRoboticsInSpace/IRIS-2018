bd28102e34152dfc1f2f6a66968a3aa005e9b73f
Pavitra122 <pavitrashadvani122@gmail.com>
2018-05-14 17:54:29 -0500

    Implemented passing arrays between the robots

--- talkietalkie/tcpCassie.py
+++ talkietalkie/tcpCassie.py
@@ -2,12 +2,32 @@
 import socket
 import json
 import threading
+import pandas as pd
+import numpy as np
 
 hostname, sld, tld, port = 'www', 'integralist', 'co.uk', 80
 target = '{}.{}.{}'.format(hostname, sld, tld)
 
 # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+map=[][]
+
+
+def initialize(mapDave):
+	map= mapDave
+
+def updateMap(mapDave):
+	for i in range(mapDave.shape[0]):
+		for j in range(mapDave.shape[1]):
+			#0 is undetected ,1 is obstacle , 2 is free
+			if(map[i][j]!=mapDave[i][j]):
+
+				if(map[i][j]== 1 || mapDave[i][j]==1):
+					map[i][j] = 1
+				if(map[i][j]=0):
+					map[i][j]=mapDave[i][j]
+
+
 
 
 def receive_messages():
@@ -15,23 +35,32 @@ def receive_messages():
     while True:
         response = client.recv(4096)
         result = json.loads(response.decode('unicode-escape').strip('"'))
-        print ('Received from: %s' % result['name'])
-        print ('Received: %s' % result['test'])
-        print ('')
+		mapDave = pd.read_json(result, orient='split')
+		mapDave = np.array(mapDave)
+		updateMap(mapDave)
+        #print ('Received from: %s' % result['name'])
+        #print ('Received: %s' % result['test'])
 
-def send_messages():
+        #print ('')
+
+def send_messages(map): #Add map parameter later, but probably not
     while True:
         # send some data (in this case a HTTP GET request)
-        user_input = input('Type something here in the console to send it to DAVE \n')
-        print ('')
-        str = '{"test": "%s", "name": "CASSIE"}' % user_input
+        #user_input = input('Type something here in the console to send it to DAVE \n')
+        #print ('')
+		map = np.array(map)
+		#map = np.array([[1,0,0],[0,1,0],[0,0,1]]) #Test identity array to send
+		str = pd.DataFrame(your_array).to_json( orient='split')
+
+        #str = '{"ar[0][0]": "%s", "name": "CASSIE"}' % user_input
         jason = json.dumps(str)
         client.sendall(jason.encode())
 
 
 # connect the client
 # client.connect((target, port))
-client.connect(('192.168.7.105', 9999))
+initialize([[1,1],[1,1]])
+client.connect(('10.192.91.11', 9999))
 print ('Connected!')
 
 server_handler = threading.Thread(
--- talkietalkie/tcpDave.py
+++ talkietalkie/tcpDave.py
@@ -1,9 +1,9 @@
-#! python3
+#!python3
 import socket
 import threading
 import json
 
-bind_ip = '192.168.7.105'
+bind_ip = '10.192.91.11'
 bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,24 +11,45 @@ server.bind((bind_ip, bind_port))
 server.listen(5)  # max backlog of connections
 
 print ('Listening on {}:{}'.format(bind_ip, bind_port))
+map=[][]
+
+def initialize(mapCassie):
+	map= mapCassie
+
+def updateMap(mapDave):
+	for i in range(mapDave.shape[0]):
+		for j in range(mapDave.shape[1]):
+			#0 is undetected ,1 is obstacle , 2 is free
+			if(map[i][j]!=mapDave[i][j]):
+
+				if(map[i][j]== 1 || mapDave[i][j]==1):
+					map[i][j] = 1
+				if(map[i][j]=0):
+					map[i][j]=mapDave[i][j]
 
 
 def handle_client_connection(client_socket):
     while True:
         request = client_socket.recv(1024)
         result = json.loads(request.decode('unicode-escape').strip('"'))
-        print ('Received from: {}'.format(result['name']))
-        print ('Received: {}'.format(result['test']))
-        print ('')
+		mapCassie = pd.read_json(result, orient='split')
+		mapCassie = np.array(mapCassie)
+		updateMap(mapCassie)
+        #print ('Received from: {}'.format(result['name']))
+        #print ('Received: {}'.format(result['test']))
+        #print ('')
 
-def send_something(client_socket):
+def send_something(client_socket,map):
     while True:
-        user_input = input('Type something here in the console to send it to CASSIE \n')
-        print ('')
-        str = '{"test": "%s", "name": "DAVE"}' % user_input
+        #user_input = input('Type something here in the console to send it to CASSIE \n')
+        #print ('')
+        #str = '{"test": "%s", "name": "DAVE"}' % user_input
+		map = np.array(map)
+		str = pd.DataFrame(your_array).to_json('data.json', orient='split')
         jason = json.dumps(str)
         client_socket.sendall(jason.encode())
 
+#initialize([[1,1],[1,1]])
 while True:
     client_sock, address = server.accept()
     print ('Accepted connection from {}:{}'.format(address[0], address[1]))
