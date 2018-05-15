fa5077b7ffa1701f95e13750e58950e0d73a3be8
Dhruv Patel <akido2398@gmail.com>
2018-05-15 01:09:43 -0500

    More joy test

--- IRIS-2017-code/joy/joystick/test.cc
+++ IRIS-2017-code/joy/joystick/test.cc
@@ -52,6 +52,9 @@ int main(int argc, char** argv)
           //event.number,
           //event.value == 0 ? "up" : "down");
         //A=0;B=1;X=2;Y=3;
+      printf("Button %u is %s\n",
+        event.number,
+        event.value == 0 ? "up" : "down");
         if(event.number==5)
           uncapped=event.value!=0?1:0;
         if(event.number==0)//A
@@ -70,6 +73,7 @@ int main(int argc, char** argv)
       else if (event.isAxis())
       {
         //printf("Axis %u is at position %d\n", event.number, event.value);
+        printf("Axis %u is at position %d\n", event.number, event.value);
         int abb = (event.value>0?1:-1)*max(0,(event.value>0?event.value:-event.value)-100*64);
 		if(event.number==1)
 			last_left=max(-1000.,min(1000.,-abb/(uncapped?25:50)));
